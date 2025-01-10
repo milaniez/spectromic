ac3793f8dbddd3f0f35fdf0f3b7b92f3e404fb99
Mehdi Zeinali <6857194+milaniez@users.noreply.github.com>
2025-01-09 17:40:41 -0800

    added time axis values: buggy

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 import queue
 import tkinter as tk
 from tkinter import simpledialog
+from datetime import datetime, timedelta
 
 # Function to list all available input devices
 def list_audio_devices():
@@ -38,14 +39,14 @@ def audio_callback(indata, frames, time, status):
 def start_spectrogram(device_id):
     plt.ion()
     fig, ax = plt.subplots()
-    x = np.linspace(0, 1, 100)
+    time_window = 10  # Display the last 10 seconds
+    start_time = datetime.now()
     y = np.linspace(0, 8000, 400)
-    X, Y = np.meshgrid(x, y)
-    Z = np.zeros_like(X)
+    Z = np.zeros((400, time_window * 100))
 
-    img = ax.imshow(Z, aspect='auto', origin='lower', extent=[0, 1, 0, 8000], cmap='magma')
+    img = ax.imshow(Z, aspect='auto', origin='lower', extent=[0, time_window, 0, 8000], cmap='magma')
     plt.colorbar(img)
-    ax.set_xlabel('Time (s)')
+    ax.set_xlabel('Time')
     ax.set_ylabel('Frequency (Hz)')
 
     with sd.InputStream(device=device_id, channels=1, callback=audio_callback):
@@ -55,8 +56,14 @@ def start_spectrogram(device_id):
                 spectrum = np.abs(np.fft.rfft(data[:, 0]))
                 spectrum = np.interp(np.linspace(0, len(spectrum), 400), np.arange(len(spectrum)), spectrum)
                 Z[:, :-1] = Z[:, 1:]
-                print("spectrum length", len(spectrum))
-                Z[:, -1] = spectrum[:400]
+                Z[:, -1] = spectrum
+
+                # Update the x-axis to show real local time
+                current_time = datetime.now()
+                time_labels = [(start_time + timedelta(seconds=i)).strftime("%H:%M:%S") for i in range(-time_window, 0)]
+                ax.set_xticks(np.linspace(0, time_window, len(time_labels)))
+                ax.set_xticklabels(time_labels, rotation=45)
+
                 img.set_data(Z)
                 plt.pause(0.01)
             except KeyboardInterrupt:
